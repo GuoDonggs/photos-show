7fed6eb8f4686d1d8dc2704da3c53be60b4d1620
Guodonggs <2818324149@qq.com>
2024-04-26 13:25:57 +0800

    添加root用户确认图片无违规可以放出的api

--- src/main/java/com/owofurry/furry/img/controller/admin/FileControlController.java
+++ src/main/java/com/owofurry/furry/img/controller/admin/FileControlController.java
@@ -1,6 +1,6 @@
 package com.owofurry.furry.img.controller.admin;
 
-import com.owofurry.furry.img.service.FileRecordService;
+import com.owofurry.furry.img.service.AdminFileRecordService;
 import com.owofurry.furry.img.utils.RUtil;
 import com.owofurry.furry.img.vo.R;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,15 +11,25 @@ import org.springframework.web.bind.annotation.*;
 @PreAuthorize("hasRole('root')")
 public class FileControlController {
 
-    FileRecordService fileRecordService;
+    AdminFileRecordService fileRecordService;
 
-    public FileControlController(FileRecordService fileRecordService) {
+    public FileControlController(AdminFileRecordService fileRecordService) {
         this.fileRecordService = fileRecordService;
     }
 
-    @GetMapping("/list/no_check/{page}")
-    public R listNotCheck(@PathVariable("page") int page) {
-        return RUtil.ok();
+    @GetMapping("/list/no_check")
+    public R listNotCheck(@RequestParam("page") int page,
+                          @RequestParam("size") int size) {
+        return fileRecordService.listNotCheck(page, size);
+    }
+
+    @PostMapping("/checked")
+    public R checked(@RequestParam("fileId") long fileId) {
+        int r = fileRecordService.checked(fileId);
+        if (r == 1) {
+            return RUtil.ok();
+        }
+        return RUtil.error(RUtil.FAILED_CODE, "操作失败，操作受影响数量为 " + r);
     }
 
     @DeleteMapping("/del/{fileId}")
