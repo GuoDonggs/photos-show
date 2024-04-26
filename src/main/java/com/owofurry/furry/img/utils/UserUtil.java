9557b38ef8a2bb7ac83bf9df43aac5a083718fdf
Guodonggs <2818324149@qq.com>
2024-04-26 13:25:00 +0800

    封装root用户判断方法

--- src/main/java/com/owofurry/furry/img/utils/UserUtil.java
+++ src/main/java/com/owofurry/furry/img/utils/UserUtil.java
@@ -10,4 +10,10 @@ public class UserUtil {
     public static String getMail() {
         return SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
     }
+
+    public static boolean isAdmin() {
+        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream().anyMatch(
+                e -> e.getAuthority().contains("root")
+        );
+    }
 }
